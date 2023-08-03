97fba94b2007dfa67b552305fcc1ca4abfb87afb
Kirstin Penzes <kpenzes17@gmail.com>
2023-08-03 12:39:03 -0400

    Completed lessons on dynamic expressions and iterating thru arrays

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,36 @@
 export default function HomePage() {
+  const appTitle = 'My Movie Library'
+  const hasMoreMovies = true
+  const displayTitle = true
+  // const uppercaseTitle = false
+  // const displayEndMessage = true
+  const seeMoreDomain = 'www.themoviedb.org'
+
+  const movies = [
+    { id: 0, title: 'Indiana Jones and the Dial of Destiny' },
+    { id: 1, title: 'Guardians of the Galaxy Vol. 3' },
+    { id: 2, title: 'Mission: Impossible - Dead Reckoning Part One' },
+    { id: 3, title: 'Spider-Man: Across the Spider-Verse', featured: true },
+    { id: 4, title: 'The Super Mario Bros. Movie' },
+  ]
+
+
   return (
     <main>
-      <h1>My Movie Library</h1>
+
+      <h1>{displayTitle && appTitle}</h1>
       <p>Here is a list of popular movies:</p>
       <ul className="movie-list">
-        <li className="featured">Indiana Jones and the Dial of Destiny</li>
-        <li>Guardians of the Galaxy Vol. 3</li>
-        <li>Mission: Impossible - Dead Reckoning Part One</li>
-        <li>Spider-Man: Across the Spider-Verse</li>
-        <li>The Super Mario Bros. Movie</li>
+        {movies.map((movie) => (
+          <li key={movie.id} className='featured'>{movie.title}</li>
+        ))}
       </ul>
+
       <p>
-        <a href="https://www.themoviedb.org/">See more</a>
+        {hasMoreMovies ? (
+          <a href="https://www.themoviedb.org/">See more</a>)
+          : ('Nothing more to see here.')
+        }
       </p>
     </main>
   )
