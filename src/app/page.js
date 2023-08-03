dbbd60e91e2a2b21e3d0395b6c0b1d908b68eab0
Kirstin Penzes <kpenzes17@gmail.com>
2023-08-03 13:16:46 -0400

    Halfway thru component lesson

--- src/app/page.js
+++ src/app/page.js
@@ -5,26 +5,21 @@ export default function HomePage() {
   // const uppercaseTitle = false
   // const displayEndMessage = true
   const seeMoreDomain = 'www.themoviedb.org'
-
   const movies = [
     { id: 0, title: 'Indiana Jones and the Dial of Destiny' },
     { id: 1, title: 'Guardians of the Galaxy Vol. 3' },
     { id: 2, title: 'Mission: Impossible - Dead Reckoning Part One' },
     { id: 3, title: 'Spider-Man: Across the Spider-Verse', featured: true },
-    { id: 4, title: 'The Super Mario Bros. Movie' },
+    { id: 4, title: 'The Super Mario Bros. Movie' }
   ]
-
 
   return (
     <main>
 
       <h1>{displayTitle && appTitle}</h1>
       <p>Here is a list of popular movies:</p>
-      <ul className="movie-list">
-        {movies.map((movie) => (
-          <li key={movie.id} className='featured'>{movie.title}</li>
-        ))}
-      </ul>
+
+      <MovieList movies={movies}/>
 
       <p>
         {hasMoreMovies ? (
@@ -33,5 +28,17 @@ export default function HomePage() {
         }
       </p>
     </main>
+  )
+}
+
+
+function MovieList(props) {
+
+  return (
+    <ul className="movie-list">
+      {props.movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
   )
 }
